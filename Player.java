b3c4f95e1fbef57b530b7a8bd2a5833af9e23374
charlottereiber <68261288+charlotte-reiber@users.noreply.github.com>
2022-05-23 21:33:58 +0000

    Working on player

--- Player.java
+++ Player.java
@@ -3,8 +3,13 @@ import java.util.ArrayList;
 class Player {
   private String name;
   private ArrayList<Card> hand = new ArrayList<Card>();
+  private int numPairs = 0;
   
   public Player(String name) {
-    
+    this.name = name;
+  }
+
+  public String askPlayer() {
+    return "hi";
   }
 }
