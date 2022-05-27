04a2bf6c4f6af366d12ed0805bc45c48c8a3f826
charlottereiber <68261288+charlotte-reiber@users.noreply.github.com>
2022-05-27 19:20:17 +0000

    Working on player

--- Player.java
+++ Player.java
@@ -8,10 +8,8 @@ class Player {
   private int numPairs = 0;
   
   public Player(String name, Scanner scan) {
-    name.replaceAll("\\s", "_");
-    name.toLowerCase();
-    this.name = name;
     this.scan = scan;
+    this.name = askName();
   }
 
   /**
@@ -47,26 +45,72 @@ class Player {
     else throw new IllegalArgumentException();
   }
 
+  /**
+  * this is a method that adds a card 
+  * to the player's hand
+  * @param the card being added
+  */
   public void getCard(Card card) {
-    // adds a card to the players hand
+    this.hand.add(card);
   }
 
+  /**
+  * this is a method that gets a value from the user
+  * @return the value of the card being looked for
+  */
   public int askValue() {
-    // returns the card value the player is asking for
-    return 5;
+    boolean gotValue = false;
+    int value = -1;
+    String token;
+    do {
+      System.out.print("What value are you looking for? ");
+      token = this.scan.next();
+      try {
+        value = Integer.valueOf(token);
+      } catch (NumberFormatException exception) {
+        token = token.substring(0,1).toLowerCase();
+        if (token.equals("a")) value = 1;
+        else if (token.equals("j")) value = 11;
+        else if (token.equals("q")) value = 12;
+        else if (token.equals("k")) value = 13;
+        if (value == -1) System.out.println("That is not a valid value.");
+        else if (!hasValue(value)) System.out.println("You do not have that value in your hand.");
+        else gotValue = true;
+      }
+    } while (!gotValue);
+    return value;
   }
 
+  /**
+  * this is a method that checks if the player
+  * is the player being asked
+  * @param the name being checked
+  * @return if the player has that name
+  */
   public boolean checkName(String name) {
-    // returns if the player is the player being asked
-    return true;
+    return this.name.equals(name);
   }
 
   /**
   * returns true if the player has a card with that value
   * @param value the value being looked for
   */
-  public boolean checkValue(int value) {
-    // returns true if the player has a card with that value
-    return true;
+  public boolean hasValue(int value) {
+    for(Card card : this.hand) {
+      if (card.getValue() == value) return true;
+    }
+    return false;
+  }
+
+  /**
+  * this is a method that gets the name of the player object
+  * @returns the name of the player
+  */
+  private String askName() {
+    System.out.print("What would you like your name to be? ");
+    String token = this.scan.nextLine();
+    token.replaceAll("\\s", "_");
+    token.toLowerCase();
+    return token;
   }
 }
