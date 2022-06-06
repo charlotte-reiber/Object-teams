284232422818fe49a1b6e027c374d0c659469e7d
charlottereiber <68261288+charlotte-reiber@users.noreply.github.com>
2022-06-06 19:37:50 +0000

    Everything works except giveCard and askValue for player object

--- Main.java
+++ Main.java
@@ -2,8 +2,31 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    Game game = new Game(new Scanner(System.in), 3);
+    Player player = new Player(new Scanner(System.in));
+    Card two = new Card (1);
+    Card three = new Card (2);
+    Card four = new Card (3);
+    Card five = new Card (4);
+    Card six = new Card (5);
+    Card threeHeart = new Card (15);
+    Card fourHeart = new Card (16);
+
+    player.goFish(two);
+    player.goFish(fourHeart);
+    player.goFish(three);
+    player.goFish(four);
+    player.goFish(five);
+    player.goFish(six);
+    player.goFish(threeHeart);
     
-    game.playGame();
+    System.out.println(player.getHand());
+    
+    System.out.println(player.hasValue(3));
+    
+    System.out.println(player.hasValue(10));
+    
+    
+    
+    
   }
 }
--- Player.java
+++ Player.java
@@ -17,6 +17,7 @@ class Player {
   * who they would like to ask during their turn
   * @returns a name
   */
+  // works
   public String askPlayer() {
     System.out.print("Who would you like to ask? ");
     String player = this.scan.nextLine();
@@ -32,6 +33,7 @@ class Player {
   * @return the card
   * @throw if the card isn't in the hand
   */
+  // does not work
   public Card giveCard(int value) throws IllegalArgumentException{
     Card match = null;
     boolean foundMatch = false;
@@ -51,6 +53,7 @@ class Player {
   * to the player's hand
   * @param the card being added
   */
+  // works
   public void goFish(Card card) {
     this.hand.add(card);
   }
@@ -59,6 +62,7 @@ class Player {
   * this is a method that gets a value from the user
   * @return the value of the card being looked for
   */
+  //does not work
   public int askValue() {
     boolean gotValue = false;
     int value = -1;
@@ -88,7 +92,10 @@ class Player {
   * @param the name being checked
   * @return if the player has that name
   */
+  // works
   public boolean checkName(String name) {
+    name = name.replaceAll("\\s", "_");
+    name = name.toLowerCase();
     return this.name.equals(name);
   }
 
@@ -96,6 +103,7 @@ class Player {
   * returns true if the player has a card with that value
   * @param value the value being looked for
   */
+  // works
   public boolean hasValue(int value) {
     for(Card card : this.hand) {
       if (card.getValue() == value) return true;
@@ -107,12 +115,13 @@ class Player {
   * this is a method that gets the name of the player object
   * @returns the name of the player
   */
-  private String askName() {
+  // works
+  public String askName() {
     System.out.print("What would you like your name to be? ");
-    String token = this.scan.nextLine();
-    token.replaceAll("\\s", "_");
-    token.toLowerCase();
-    return token;
+    String player = this.scan.nextLine();
+    player = player.replaceAll("\\s", "_");
+    player = player.toLowerCase();
+    return player;
   }
 
   /**
@@ -120,12 +129,13 @@ class Player {
   * if the player has any matches
   * @returns the number of pairs the player has
   */
+  // works
   public int checkPairs() {
     for (int i = 0; i < this.hand.size(); i++) {
       for (int j = i; j < this.hand.size(); j++) {
         int a = this.hand.get(i).getValue();
         int b = this.hand.get(j).getValue();
-        if (a == b || i != j) {
+        if (a == b && i != j) {
           this.numPairs++;
           this.hand.remove(j);
           this.hand.remove(i);
@@ -142,9 +152,10 @@ class Player {
   * representing the player's hand
   * @returns the player's hand
   */
+  // works
   public String getHand() {
     String hand = "";
-    for (Card card : this.hand) hand += card.toString() + "";
+    for (Card card : this.hand) hand += card.toString() + ", ";
     return hand;
   }
 
@@ -152,6 +163,7 @@ class Player {
   * this is a method that tells if the player has cards
   * @returns if the player has cards
   */
+  // works
   public boolean hasCards() {
     return this.hand.size() > 0;
   }
@@ -160,6 +172,7 @@ class Player {
   * this is a method that returns the player's name
   * @returns the player's name
   */
+  // works
   public String getName() {
     return this.name;
   }
@@ -170,6 +183,7 @@ class Player {
   * @returns if the players are the same
   * @param the player being checked against
   */
+  // works
   public boolean equals(Player player) {
     if (!checkName(player.name)) return false;
     else if (!this.getHand().equals(player.getHand()))
@@ -181,6 +195,7 @@ class Player {
   * this is a method that returns 
   * the number of pairs
   */
+  // works
   public int getScore() {
     return this.numPairs;
   }
